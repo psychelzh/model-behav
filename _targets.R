98e4382787318e391976dc1b3740b50c42ab2823
Liang Zhang <psychelzh@outlook.com>
2022-11-17 10:39:28 +0800

    Add keepTrack
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _targets.R
+++ _targets.R
@@ -2,19 +2,13 @@ library(targets)
 tar_option_set(packages = "tidyverse")
 tar_source()
 future::plan(future.callr::callr)
-config <- readr::read_csv("config.csv", show_col_types = FALSE) |>
+config <- readr::read_csv("config/task_preproc.csv", show_col_types = FALSE) |>
   tidyr::drop_na() |>
   dplyr::mutate(preproc = rlang::syms(paste0("preproc_", preproc)))
 static_branches <- tarchetypes::tar_map(
   config,
   names = task,
   list(
-    tarchetypes::tar_file_read(
-      data,
-      fs::path("data", sprintf("%s.arrow", task)),
-      read = arrow::read_feather(!!.x)
-    ),
-    tar_target(data_clean, screen_data(data)),
     tar_target(
       indices,
       preproc(data_clean) |>
@@ -24,18 +18,51 @@ static_branches <- tarchetypes::tar_map(
           values_to = "score"
         ) |>
         select(-task_datetime)
-    )
+    ),
+    tarchetypes::tar_file_read(
+      data,
+      fs::path("data", sprintf("%s.arrow", task)),
+      read = arrow::read_feather(!!.x)
+    ),
+    tar_target(data_clean, screen_data(data))
   )
 )
 list(
+  tarchetypes::tar_file_read(
+    task_indices,
+    "config/task_indices.csv",
+    read = read_csv(!!.x, show_col_types = FALSE)
+  ),
+  tarchetypes::tar_file_read(
+    indices_keepTrack,
+    "data/keepTrack_4scores_all.csv",
+    read = read_csv(!!.x, show_col_types = FALSE) |>
+      rowwise() |>
+      mutate(score = sum(c_across(starts_with("score")))) |>
+      ungroup() |>
+      transmute(
+        name = "keepTrack",
+        sub_id = ID,
+        index = "score",
+        score
+      ) |>
+      group_by(sub_id) |>
+      filter(row_number() == 1) |>
+      ungroup()
+  ),
   static_branches,
   tarchetypes::tar_combine(
     indices,
-    static_branches[[4]],
+    static_branches[[1]],
     command = bind_rows(
       !!!.x,
       .id = "name"
     ) |>
-      mutate(name = str_remove(name, "indices_"))
+      mutate(name = str_remove(name, "indices_")) |>
+      semi_join(
+        filter(task_indices, selected),
+        by = c("name", "index")
+      ) |>
+      bind_rows(indices_keepTrack)
   )
 )
